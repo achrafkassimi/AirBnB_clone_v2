dc11d6b6ce9b1d62856bc73cf3b8438521c6f743
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-21 17:41:19 +0100

    fix task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -6,20 +6,24 @@ starts a Flask web application
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -27,10 +31,12 @@ def python_is_cool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def im_a_number(n):
     """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
