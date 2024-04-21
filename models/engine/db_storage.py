e0e4738cabb334f1ea4e3f9cd396b64086c1efd0
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-21 19:37:32 +0100

    test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,8 +13,8 @@ from models.review import Review
 from models.amenity import Amenity
 
 
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+# classes = {"Amenity": Amenity, "City": City,
+#            "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class DBStorage:
@@ -40,15 +40,29 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """query on the current database session"""
-        new_dict = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
-                for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    new_dict[key] = obj
-        return (new_dict)
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__,
+                                     elem.id)
+                dic[key] = elem
+        else:
+            lista = [State, City, User,
+                     Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__,
+                                         elem.id)
+                    dic[key] = elem
+        return (dic)
 
     def new(self, obj):
         """add a new element in the table
