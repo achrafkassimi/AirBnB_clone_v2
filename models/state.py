11c9b79bdc7a8982530a4621b1419be995eb030b
Kassimi Achraf <38761929+achrafkassimi@users.noreply.github.com>
2024-04-21 16:53:28 +0100

    my test commit

---
+++ models/state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+import shlex
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+import models
+from models.base_model import BaseModel, Base
+
+
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship('City',
+                          backref='state',
+                          cascade='all, delete-orphan')
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
