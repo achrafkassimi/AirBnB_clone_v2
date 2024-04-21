cab1285aa169b68d4f954d2388d9d61f9537d05b
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-21 18:04:30 +0100

    task 7 fix

--- models/state.py
+++ models/state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import shlex
-from sqlalchemy import Column, ForeignKey, String
-from sqlalchemy.orm import relationship
 import models
 from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
