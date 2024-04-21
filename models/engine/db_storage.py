bf794312b8416fe9560e7bab6e46cde7b59c0b8d
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-21 21:02:57 +0100

    mmj

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -50,42 +50,42 @@ class DBStorage:
         Return:
             Dict of queried classes in the format <class name>.<obj id> = obj.
         """
-        if cls is None:
-            objs = self.__session.query(State).all()
-            objs.extend(self.__session.query(City).all())
-            objs.extend(self.__session.query(User).all())
-            objs.extend(self.__session.query(Place).all())
-            objs.extend(self.__session.query(Review).all())
-            objs.extend(self.__session.query(Amenity).all())
-        else:
-            if type(cls) == str:
-                cls = eval(cls)
-            objs = self.__session.query(cls)
-        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
-
-        # """returns a dictionary
-        # Return:
-        #     returns a dictionary of __object
-        # """
-        # dic = {}
-        # if cls:
-        #     if type(cls) is str:
-        #         cls = eval(cls)
-        #     query = self.__session.query(cls)
-        #     for elem in query:
-        #         key = "{}.{}".format(type(elem).__name__,
-        #                              elem.id)
-        #         dic[key] = elem
+        # if cls is None:
+        #     objs = self.__session.query(State).all()
+        #     objs.extend(self.__session.query(City).all())
+        #     objs.extend(self.__session.query(User).all())
+        #     objs.extend(self.__session.query(Place).all())
+        #     objs.extend(self.__session.query(Review).all())
+        #     objs.extend(self.__session.query(Amenity).all())
         # else:
-        #     lista = [State, City, User,
-        #              Place, Review, Amenity]
-        #     for clase in lista:
-        #         query = self.__session.query(clase)
-        #         for elem in query:
-        #             key = "{}.{}".format(type(elem).__name__,
-        #                                  elem.id)
-        #             dic[key] = elem
-        # return (dic)
+        #     if type(cls) == str:
+        #         cls = eval(cls)
+        #     objs = self.__session.query(cls)
+        # return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
+
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__,
+                                     elem.id)
+                dic[key] = elem
+        else:
+            lista = [State, City, User,
+                     Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__,
+                                         elem.id)
+                    dic[key] = elem
+        return (dic)
 
     def new(self, obj):
         """add a new element in the table
