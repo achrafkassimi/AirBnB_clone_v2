ba539f30d830e56e94fcb94439e6b86456dcc514
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-22 03:08:39 +0100

    7854

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,21 +5,20 @@ starts a Flask web application
 
 from flask import Flask, render_template
 from models import *
-import models
 from models import storage
-
-print(models.storage)
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    # states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    """
+    display a HTML page with the states listed 
+    in alphabetical order
+    """
     states = storage.all(State)
     states_list = list(states.values())
-    # print(states,states_list)
-    return render_template('7-states_list.html', states=states_list)
+    return render_template('7-states_list.html', 
+                           states=states_list)
 
 
 @app.teardown_appcontext
