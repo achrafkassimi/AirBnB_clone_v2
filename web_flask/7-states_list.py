bea978c14fb9daa1cc275598212df7dfe4586776
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-25 20:39:44 +0100

    fix roteur

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,7 +6,6 @@ starts a Flask web application
 from flask import Flask, render_template
 from models import *
 from models import storage
-from models import State
 app = Flask(__name__)
 
 
@@ -16,11 +15,10 @@ def states_list():
     display a HTML page with the states listed 
     in alphabetical order
     """
-    states = storage.all(State).values()
-    print(states)
-    # states_list = list(states.values())
+    states = storage.all(State)
+    states_list = list(states.values())
     return render_template('7-states_list.html', 
-                           states=states)
+                           states=states_list)
 
 
 @app.teardown_appcontext
