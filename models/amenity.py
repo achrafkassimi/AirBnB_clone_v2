1012eb0a163f2311c652cb8c4fdee6663b7ee071
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-25 19:17:41 +0100

    fix class amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-# from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-# from models.place import place_amenity
+from models.place import place_amenity
 from os import getenv
 import models
 
@@ -18,7 +18,7 @@ class Amenity(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        # place_amenities = relationship("Place", secondary=place_amenity)
+        place_amenities = relationship("Place", secondary=place_amenity)
     else:
         name = ""
 
