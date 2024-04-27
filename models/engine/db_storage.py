d6e880ad73d3c295d6adce83e458388876f5348e
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-27 17:42:55 +0100

    fix encoding=latin1

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -40,7 +40,8 @@ class DBStorage:
                                       format(HBNB_MYSQL_USER,
                                              HBNB_MYSQL_PWD,
                                              HBNB_MYSQL_HOST,
-                                             HBNB_MYSQL_DB), echo=True)
+                                             HBNB_MYSQL_DB), echo=True,
+                                             encoding='latin1')
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
