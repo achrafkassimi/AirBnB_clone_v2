7e418ca94da7a6ea9c533a6c9189b59c223f8f2e
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-27 19:00:29 +0100

    fix 3awd 2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -40,7 +40,8 @@ class DBStorage:
         self.__engine = create_engine(
             'mysql+mysqldb://{}:{}@{}/{}?charset=latin1'
             .format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD,
-                    HBNB_MYSQL_HOST, HBNB_MYSQL_DB), pool_pre_ping=True)
+                    HBNB_MYSQL_HOST, HBNB_MYSQL_DB))
+        # , pool_pre_ping=True
 
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
