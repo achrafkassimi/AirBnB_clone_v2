d1c2a775b77b591cd4d63399710dd0acadf22346
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-27 17:53:44 +0100

    latin1 fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -36,12 +36,11 @@ class DBStorage:
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}?charset=latin1'.
                                       format(HBNB_MYSQL_USER,
                                              HBNB_MYSQL_PWD,
                                              HBNB_MYSQL_HOST,
-                                             HBNB_MYSQL_DB), echo=True,
-                                             encoding='latin1')
+                                             HBNB_MYSQL_DB), echo=True)
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
