e2764064bd44dca5d271ffe0a5f1a931dc58fd2f
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-27 16:44:34 +0100

    fix task 8 classe

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -15,7 +15,7 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-classes = {"Amenity": Amenity, "City": City,
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
 
